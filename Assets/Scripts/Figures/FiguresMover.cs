5c796d4a20ddbdd57bfcc0a1cef36de1f7cff18b
Maryia Ondra <ya.ondra@yandex.by>
2021-01-16 23:37:04 +0300

    Initial commit

---
+++ Assets/Scripts/Figures/FiguresMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiguresMover : MonoBehaviour
+{
+    [SerializeField] private GameObject[] usedPrefabs;
+    [SerializeField] private Material material;
+
+    private UsedFigure[] workPrefabs;
+    int randomNumbr;
+
+    private void Awake()
+    {
+        for (int i = 0; i < usedPrefabs.Length; i++)
+        {
+            GameObject.Instantiate(usedPrefabs[i], transform).SetActive(false);
+        }
+
+        workPrefabs = GetComponentsInChildren<UsedFigure>(true);
+    }
+
+    private void Start()
+    {
+        SetRandomPrefab();
+    }
+
+    public void SetRandomPrefab() 
+    {
+        randomNumbr = Random.Range(0, usedPrefabs.Length);
+        workPrefabs[randomNumbr].Activate();
+    }
+    
+    private void SetRandomColor() 
+    {
+    
+    }
+
+    private void SetRandomRotation() 
+    {
+    
+    }
+
+    public void Answer() 
+    {
+        workPrefabs[randomNumbr].DropDown();
+    }
+}
---
+++ Assets/Scripts/Figures/UsedFigure.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class UsedFigure : MonoBehaviour
+{
+    public Color color { get; set; }
+    public GameObject prefab { get; set; }
+    public float rotation { get; set; }
+
+    private Rigidbody rigidbody;
+    private float speed = 0.02f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        do
+        {
+            rigidbody.AddForce(Vector3.left * Time.deltaTime);
+        }
+        while (transform.position.x != 0);        
+    }
+
+    private void Update()
+    {
+        //if (transform.position.x != 0)
+        //{
+        //    rigidbody.AddForce(Vector3.left * speed * Time.deltaTime);
+        //}
+
+        if (transform.position.y < -5)
+        {
+            Deactivate();
+            ResetPosition();
+        }
+    }
+
+    public void Activate() 
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void DropDown() 
+    {
+        rigidbody.isKinematic = false;
+        rigidbody.useGravity = true;
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    public void ResetPosition() 
+    {
+        transform.position = transform.parent.position;
+    }
+}
